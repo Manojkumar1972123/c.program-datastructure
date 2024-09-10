8593b78fbfd253cad0d6f7a1f850256c54da2bbb
Manojkumar1972123 <manojkumarhm08@gmail.com>
2024-09-10 03:26:09 -0700

    Create odd even.c

---
+++ odd even.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  int manoj;
+  printf("Enter the number");
+  scanf("%d",&manoj);
+  if (manoj%2==0) {
+    printf("even");
+  }
+  else {
+    printf("odd");
+  }
+}
+
