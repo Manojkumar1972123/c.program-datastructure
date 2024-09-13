589107e50edf56f9d6ea4ac0e144a7e14bd7c347
Manojkumar1972123 <manojkumarhm08@gmail.com>
2024-09-13 03:44:36 -0700

    Create string3.c

---
+++ string3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+  int n;
+  char str[10];
+  printf("enter your name:");
+  scanf("%d",str);
+  
+  printf("Hello %s, welcome to c programming.", str);
+  
+    
+    
+    
+}
